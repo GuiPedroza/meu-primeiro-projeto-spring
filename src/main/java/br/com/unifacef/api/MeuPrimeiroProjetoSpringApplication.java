fbfa6df618ebdeb4486b9a5cb34399eb8259d30a
guilherme <guipedrosa198@gmail.com>
2019-08-28 00:32:04 -0300

    segurança criptografia de senhas

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,7 +1,11 @@
 package br.com.unifacef.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.unifacef.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
@@ -11,4 +15,14 @@ public class MeuPrimeiroProjetoSpringApplication {
 		System.out.println("Esta é uma alteração de projeto");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente:" + senhaEncoded);
+			System.out.println("Senha v�lida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
 }
